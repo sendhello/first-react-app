80be1a88cdaad0111862f6692af478878844de56
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2021-01-24 17:57:24 +0300

    Примеры динамического содержания

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 export const Country = () => {
-    return(
+    let a = 56
+    let arr = [1, 2, 3, 4]
+
+    return (
         <div>
-            <h2>I listen JS React</h2>
+            <h2>Test view computation</h2>
+            <div style={{fontSize: '10px', color: 'gray'}}>
+                <h3>Number 1: 12 + 65 = {12 + 65}</h3>
+                <h3>Number 3: Math() = {Math.round(Math.random() * 100 / 3)}</h3>
+                <h3>Number 3: a = {a}</h3>
+                <h3>Number 4: arr[2] = {arr[2]}</h3>
+            </div>
         </div>
     )
 }
