f16f52c2a42137fc6af0e94888af768ed1bbc3ab
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2021-01-24 21:43:28 +0300

    Добавления стейта

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import {Country} from './components/Country'
 
 class App extends React.Component {
+    state = {
+        countries: [
+            {name: 'Россия', capital: 'Москва'},
+            {name: 'США', capital: 'Вашингтон'},
+            {name: 'Канада', capital: 'Октава'},
+            {name: 'Кипр', capital: 'Кипр'},
+            {name: 'Австралия', capital: 'Канберра'}
+        ]
+    }
+
     render() {
-        const style = {
-            textAlign: 'center',
-            color: '#ff0000',
-            fontWeight: '500',
-            borderBottom: '#f00 1px solid'
-        }
+        console.log(this.state)
+        let countries = this.state.countries
 
         return (
-            <div className="App" style={style}>
+            <div className="App" style={{display: 'block'}}>
                 <h1>Hello React!!!</h1>
-                <div style={{color: '#00f', border: 'none'}}>
-                    <Country/>
-                </div>
+                <Country name={countries[0].name} capital={countries[0].capital}/>
+                <Country name={countries[1].name} capital={countries[1].capital}/>
+                <Country name={countries[2].name} capital={countries[2].capital}/>
+                <Country name={countries[3].name} capital={countries[3].capital}/>
+                <Country name={countries[4].name} capital={countries[4].capital}/>
             </div>
         )
     }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-export const Country = () => {
-    let a = 56
-    let arr = [1, 2, 3, 4]
+export const Country = ({name, capital}) => {
+    const style = {
+        textAlign: 'center',
+        width: '200px',
+        height: '200px',
+        padding: '10px',
+        color: 'gray',
+        fontWeight: '500',
+        border: 'red 1px solid',
+        display: 'inline-block',
+        margin: '0 5px'
+    }
 
     return (
-        <div>
-            <h2>Test view computation</h2>
-            <div style={{fontSize: '10px', color: 'gray'}}>
-                <h3>Number 1: 12 + 65 = {12 + 65}</h3>
-                <h3>Number 3: Math() = {Math.round(Math.random() * 100 / 3)}</h3>
-                <h3>Number 3: a = {a}</h3>
-                <h3>Number 4: arr[2] = {arr[2]}</h3>
-            </div>
+        <div style={style}>
+            <h2>Страна: {name}</h2>
+            <h3>Столица: {capital}</h3>
         </div>
     )
 }
