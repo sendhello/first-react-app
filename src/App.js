ed5b09063d514c7292ed5f11aca2707bdf8964f3
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2021-01-24 17:36:32 +0300

    Примеры функциональных и классовых компонентов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {Country} from './components/Country'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        const style = {
+            textAlign: 'center',
+            color: '#ff0000',
+            fontWeight: '500',
+            borderBottom: '#f00 1px solid'
+        }
+
+        return (
+            <div className="App" style={style}>
+                <h1>Hello React!!!</h1>
+                <div style={{color: '#00f', border: 'none'}}>
+                    <Country/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/Country.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Country = () => {
+    return(
+        <div>
+            <h2>I listen JS React</h2>
+        </div>
+    )
+}
